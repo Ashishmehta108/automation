e20affc361efea3d936a87afd05d5d2d2cf4a91f
Ashish mehta <ashishmehtawork108@gmail.com>
2025-09-05 17:27:18 +0530

    automation

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const puppeteer = require("puppeteer");
+const fs = require("fs");
+
 
 (async () => {
+    // 1. Load data from file (CSV simple split)
+
+
     const browser = await puppeteer.launch({
-        headless: true, // ✅ must be true on Render
+        executablePath: "/usr/bin/google-chrome", // use Render's Chrome
         args: ["--no-sandbox", "--disable-setuid-sandbox"],
+        headless: true,
     });
-
     const page = await browser.newPage();
-    await page.goto("https://youtube.com", { waitUntil: "networkidle2" });
-    console.log("✅ Opened YouTube in Render worker");
 
-    await new Promise((res) => setTimeout(res, 4000));
+    await page.goto("http://youtube.com")
+    console.log("done ")
+
     await browser.close();
 })();
