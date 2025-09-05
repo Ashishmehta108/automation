9a1243e29035811b65d5ba40b4c161cbc5dc3724
Ashish mehta <ashishmehtawork108@gmail.com>
2025-09-05 17:20:32 +0530

    automation

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const puppeteer = require("puppeteer");
 
 (async () => {
     const browser = await puppeteer.launch({
-        headless: false,
+        headless: true, // ✅ must be true on Render
         args: ["--no-sandbox", "--disable-setuid-sandbox"],
     });
 
     const page = await browser.newPage();
     await page.goto("https://youtube.com", { waitUntil: "networkidle2" });
-    console.log("✅ Opened YouTube");
+    console.log("✅ Opened YouTube in Render worker");
 
     await new Promise((res) => setTimeout(res, 4000));
     await browser.close();
