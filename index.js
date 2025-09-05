c887e113c0addcd09673bdb5e51bf60aebe056f4
Ashish mehta <ashishmehtawork108@gmail.com>
2025-09-05 18:02:10 +0530

    automation

--- index.js
+++ index.js
@@ -1,34 +1,47 @@
 const puppeteer = require("puppeteer");
+const express = require("express")
 
-(async () => {
-    const browser = await puppeteer.launch({
-        headless: true,
-        args: ["--no-sandbox", "--disable-setuid-sandbox", "--disable-dev-shm-usage"]
-    });
+const app = express()
 
-    const page = await browser.newPage();
-    await page.goto("https://www.youtube.com/", { waitUntil: "networkidle2" });
-
-    const query = "puppeteer tutorial";
-    await page.type("input.yt-searchbox-input", query);
-    await page.keyboard.press("Enter");
-
-    await page.waitForSelector("ytd-video-renderer", { timeout: 10000 });
-
-    const results = await page.evaluate(() => {
-        const videos = [];
-        document.querySelectorAll("ytd-video-renderer").forEach((el, i) => {
-            if (i < 5) {
-                const title = el.querySelector("#video-title")?.innerText || "No title";
-                const url = el.querySelector("#video-title")?.href || "No link";
-                videos.push({ title, url });
-            }
+    (async () => {
+        const browser = await puppeteer.launch({
+            headless: true,
+            args: ["--no-sandbox", "--disable-setuid-sandbox", "--disable-dev-shm-usage"]
         });
-        return videos;
-    });
 
-    console.log("🔎 Search Results:");
-    console.table(results);
+        const page = await browser.newPage();
+        await page.goto("https://www.youtube.com/", { waitUntil: "networkidle2" });
 
-    await browser.close();
-})();
+        const query = "puppeteer tutorial";
+        await page.type("input.yt-searchbox-input", query);
+        await page.keyboard.press("Enter");
+
+        await page.waitForSelector("ytd-video-renderer", { timeout: 10000 });
+
+        const results = await page.evaluate(() => {
+            const videos = [];
+            document.querySelectorAll("ytd-video-renderer").forEach((el, i) => {
+                if (i < 5) {
+                    const title = el.querySelector("#video-title")?.innerText || "No title";
+                    const url = el.querySelector("#video-title")?.href || "No link";
+                    videos.push({ title, url });
+                }
+            });
+            return videos;
+        });
+
+        console.log("🔎 Search Results:");
+        console.table(results);
+
+        await browser.close();
+    })();
+
+
+
+app.get("/health", async () => {
+    console.log("app working")
+})
+
+app.listen(4444, () => {
+    console.log("server running")
+})
